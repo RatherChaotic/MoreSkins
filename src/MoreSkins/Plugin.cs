cdee82fdcc1668a85e369fb70b3cf39621746f77
RatherChaotic <OccationallyChaotic@outlook.com>
2025-06-21 13:50:16 -0400

    Little bit of cleanup before release

--- src/MoreSkins/Plugin.cs
+++ src/MoreSkins/Plugin.cs
@@ -8,7 +8,7 @@ namespace MoreSkins;
 [BepInAutoPlugin]
 public partial class Plugin : BaseUnityPlugin
 {
-    private class CharacterAwakePatch
+    private class Patcher
     {
         private static bool CreateSkinOption(Customization customization, string name, Color color)
         {
@@ -20,22 +20,12 @@ public partial class Plugin : BaseUnityPlugin
             customization.skins = customization.skins.AddToArray(skinOption);
             return true;
         }
-
-
-        [HarmonyPatch(typeof(Character), "Awake")]
-        [HarmonyPostfix]
-        private static void CharacterAwakePostfix(Character __instance)
-        {
-            
-            CharacterCustomization.SetCharacterSkinColor(8);
-            // Your code here, runs after Character.Awake
-        }
+        
         [HarmonyPatch(typeof(PassportManager), "Awake")]
         [HarmonyPostfix]
         private static void PassportManagerAwakePostfix(PassportManager __instance)
         {
             var customization = __instance.GetComponent<Customization>();
-            Log.LogInfo("Running PassportManager Awake Patch");
             CreateSkinOption(customization, "Skin_White", Color.white);
             CreateSkinOption(customization, "Skin_Black", new Color(0.2f, 0.2f, 0.2f, 1));
             CreateSkinOption(customization, "Skin_SkinTone1", new Color32(141, 85, 36, 1));
@@ -44,23 +34,6 @@ public partial class Plugin : BaseUnityPlugin
             CreateSkinOption(customization, "Skin_SkinTone4", new Color32(241, 194, 125, 1));
             CreateSkinOption(customization, "Skin_SkinTone5", new Color32(255, 219, 172, 1));
         }
-        
-        /*[HarmonyPatch(typeof(Customization), "Awake")]
-        [HarmonyPostfix]
-        private static void CustomizationAwakePostfix(Customization __instance)
-        {
-            Log.LogInfo("Running Customization Awake Patch");
-            if (__instance.skins.Length < 10)
-            {
-                Log.LogInfo("Adding skin option to Customization");
-                var skinWhite = ScriptableObject.CreateInstance<CustomizationOption>();
-                skinWhite.color = Color.white;
-                skinWhite.name = "Skin_White";
-                skinWhite.type = Customization.Type.Skin;
-                __instance.skins = __instance.skins.AddToArray(skinWhite);
-                Log.LogInfo("Added White Skin to Customization");
-            }
-        }*/
     }
 
     internal static ManualLogSource Log;
@@ -68,7 +41,7 @@ public partial class Plugin : BaseUnityPlugin
     private void Awake()
     {
         Log = Logger;
-        _harmony.PatchAll(typeof(CharacterAwakePatch));
+        _harmony.PatchAll(typeof(Patcher));
         Log.LogInfo($"Plugin MoreSkins is loaded!");
     }
 
